5c0429fa79bcece06d005e3af1bec726a200defc
bscal <ubuntu@ip-172-31-40-152.us-east-2.compute.internal>
2019-08-24 19:43:09 +0000

    deleted unused files

--- ServerPassword/__resource.lua
+++
@@ -1,8 +0,0 @@
-------------------------------
--- Server Password, Made by --
---  FAXES & GlitchDetector  --
-------------------------------
--- Config can be found in the files!
-
-client_script "client.lua"
-server_script "server.lua"
